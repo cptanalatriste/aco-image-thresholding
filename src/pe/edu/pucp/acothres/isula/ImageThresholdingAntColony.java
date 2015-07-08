5b072f99e38b8b41e1830ce1ed4a54637fb01392
Carlos G. Gavidia <cgavidiac@gmail.com>
2015-07-08 10:16:46 -0500

    Adding pending files

---
+++ src/pe/edu/pucp/acothres/isula/ImageThresholdingAntColony.java
@@ -0,0 +1,62 @@
+package pe.edu.pucp.acothres.isula;
+
+import isula.aco.Ant;
+import isula.aco.AntColony;
+import isula.aco.Environment;
+
+import pe.edu.pucp.acosthres.image.ImagePixel;
+import pe.edu.pucp.acothres.ProblemConfiguration;
+
+import java.util.logging.Logger;
+
+public class ImageThresholdingAntColony extends AntColony<ImagePixel> {
+
+  private static Logger logger = Logger
+      .getLogger(ImageThresholdingAntColony.class.getName());
+
+  public ImageThresholdingAntColony() {
+    // The number of ants is calculated later.
+    super(0);
+  }
+
+  @Override
+  public void buildColony(Environment environment) {
+    int antCounter = 0;
+    double[][] problemGraph = environment.getProblemGraph();
+
+    for (int i = 0; i < problemGraph.length; i++) {
+      for (int j = 0; i < problemGraph[0].length; i++) {
+
+        if (problemGraph[i][j] != ProblemConfiguration.ABSENT_PIXEL_FLAG) {
+          Ant<ImagePixel> ant = this.createAnt(environment);
+          ant.getSolution()[0] = new ImagePixel(i, j,
+              environment.getProblemGraph());
+          this.getHive().add(ant);
+
+          antCounter += 1;
+        }
+      }
+    }
+
+    this.setNumberOfAnts(antCounter);
+  }
+
+  @Override
+  protected Ant<ImagePixel> createAnt(Environment environment) {
+    EnvironmentForImageThresholding env = (EnvironmentForImageThresholding) environment;
+    return new AntForImageThresholding(env.getNumberOfSteps());
+  }
+
+  @Override
+  public void clearAntSolutions() {
+    logger.info("CLEARING ANT SOLUTIONS");
+
+    for (Ant<ImagePixel> ant : getHive()) {
+      ImagePixel initialPixel = ant.getSolution()[0];
+      ant.clear();
+      ant.setCurrentIndex(0);
+      ant.visitNode(initialPixel);
+    }
+  }
+
+}
---
+++ src/pe/edu/pucp/acothres/isula/OnlinePheromoneUpdateForThresholding.java
@@ -0,0 +1,26 @@
+package pe.edu.pucp.acothres.isula;
+
+import isula.aco.ConfigurationProvider;
+import isula.aco.Environment;
+import isula.aco.algorithms.antsystem.OnlinePheromoneUpdate;
+import pe.edu.pucp.acosthres.image.ImagePixel;
+import pe.edu.pucp.acothres.ProblemConfiguration;
+
+public class OnlinePheromoneUpdateForThresholding extends
+    OnlinePheromoneUpdate<ImagePixel> {
+
+  @Override
+  protected double getNewPheromoneValue(ImagePixel solutionComponent,
+      Environment environment, ConfigurationProvider configurationProvider) {
+
+    AntForImageThresholding ant = (AntForImageThresholding) getAnt();
+
+    double contribution = 1 / (ProblemConfiguration.COST_FUNCTION_PARAMETER_A + ProblemConfiguration.COST_FUNCTION_PARAMETER_B
+        * ant.getSolutionQuality(environment));
+
+    double newValue = ant.getPheromoneTrailValue(solutionComponent,
+        ant.getCurrentIndex(), environment)
+        * ProblemConfiguration.EXTRA_WEIGHT + contribution;
+    return newValue;
+  }
+}
